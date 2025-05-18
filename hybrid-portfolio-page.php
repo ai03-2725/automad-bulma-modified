19832d2ba29e2d7fc07859118837fef9f00e07e4
ai03 <admin@ai03.com>
2025-05-18 16:18:41 +0900

    Create hybrid content-portfolio page for landing

---
+++ hybrid-portfolio-page.php
@@ -0,0 +1,45 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+<@ snippets/header.php @>
+
+<div class="main-content-div">
+	
+	<@ snippets/hero.php @>
+
+	<section class="section">
+		<div class="container content">
+			@{ text | markdown }
+		</div>
+	</section>
+	<@ newPagelist {
+		type: 'children',
+		context: @{ showPagesBelow },
+		filter: @{ ?filter },
+		search: @{ ?search },
+		sort: @{ sortPages | def ('date desc') },
+		template: @{ templateFilter }
+	} @>
+	<@ if @{ checkboxShowAllPagesInPagelist } or @{ ?search } @>
+		<@ pagelist { type: false } @>
+	<@ end @>
+	<section class="section">
+		<@ if not @{ checkboxHideTitleAndTeaser } @>
+			<div class="container content">
+				<div class="columns">
+					<div class="column is-8">
+						<h1>@{ title }</h1>
+						<div class="is-size-4">
+							@{ textTeaser | markdown }
+						</div>
+					</div>
+				</div>
+			</div>
+		<@ end @>
+		<div class="container content">
+			<@ snippets/filters.php @>
+		</div>	
+		<@ snippets/pagelist.php @>
+	</section>
+
+</div>
+	
+<@ snippets/footer.php @>
